bb2d4c9c02b0c64a802c529b6f9c1ba53b4819f4
Aquiles Lopes <aquiles_oliveira@live.com>
2022-07-13 09:43:54 -0300

    add date filter in sales

--- backend/src/main/java/br/com/gluom/dsmeta/services/SaleService.java
+++ backend/src/main/java/br/com/gluom/dsmeta/services/SaleService.java
@@ -3,8 +3,11 @@ package br.com.gluom.dsmeta.services;
 import br.com.gluom.dsmeta.entities.Sale;
 import br.com.gluom.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -13,8 +16,14 @@ public class SaleService {
     @Autowired
     private SaleRepository saleRepository;
 
-    public List<Sale> findAll() {
-        return saleRepository.findAll();
+    public Page<Sale> findAll(String minDate, String maxDate, Pageable pageable) {
+
+        LocalDate today = LocalDate.now();
+
+        LocalDate min = minDate.length() > 0 ? LocalDate.parse(minDate) : today.minusDays(30);
+        LocalDate max = maxDate.length() > 0 ? LocalDate.parse(maxDate) : today;
+
+        return saleRepository.findSales(min, max, pageable);
     }
 
     public Sale findById(Long id) {
